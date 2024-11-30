c2fb746424faf9c3335e0c07fc65537d5c8e8fe2
Sarthak Bhardwaj Dev <sarthakbhardwaj274@gmail.com>
2024-11-30 07:10:12 +0530

    created user model in mongodb

--- Backend/app.js
+++ Backend/app.js
@@ -2,13 +2,16 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors');
+const connectToDb = require('./db/db');
+
+//load env variables fro the .env file
+dotenv.config();
+
+connectToDb();
 
 //initialize the express application
 const app = express();
 app.use(cors());
-
-//load env variables fro the .env file
-dotenv.config();
 
 app.get('/',(req,res)=>{
     res.send('Hello world!')
---
+++ Backend/models/user.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        firstName: {
+            type: String,
+            required: true,
+            minlength: [3, 'First name should be at least 3 characters long'],
+        },
+        lastName: {
+            type: String,
+            minlength: [3, 'Last name should be at least 3 characters long'],
+        }
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        minlength: [5, 'Email must be atleast 5 characters long']
+    },
+    password:{
+        type: String,
+        required: true,
+        // /By setting select: false on the password field, you're telling Mongoose to not return the password field in the results of queries (such as find(), findOne(), etc.). This is typically done for security reasons so that sensitive data (like passwords) isn't returned to the client unless specifically requested.
+        select: false,
+    },
+    //to track driver and passenger's current location
+    socketId:{
+        type: String
+    },
+})
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET)
+    return token;
+}
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.statics.hashPassword = async function(){
+    return await bcrypt.hash(password,10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
+//next step - this model is being used in controllers folder
